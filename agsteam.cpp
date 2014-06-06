eefe3dd43fcfcbc2f7dfe0b00dc521ddb44b3f81
Gregor Riepl <onitake@gmail.com>
2014-06-06 18:50:46 +0200

    Implemented persistent storage for leaderboards and achievements

--- agsteam.cpp
+++ agsteam.cpp
@@ -1,5 +1,5 @@
 // AGS Steam stub plugin
-// Steam-free replacement for AGSteam by monkey (see http://www.adventuregamestudio.co.uk/forums/index.php?topic=44712.0)
+// Steam-free replacement for the AGSteam plugin (see http://www.adventuregamestudio.co.uk/forums/index.php?topic=44712.0)
 // No copyright. This is public domain.
 
 
@@ -32,6 +32,7 @@ BOOL APIENTRY DllMain(HANDLE hModule, DWORD ulReason, LPVOID lpReserved) {
 
 #define AGSTEAM_PLUGIN_NAME "AGSteamStub"
 #define AGSTEAM_MESSAGE_PREFIX AGSTEAM_PLUGIN_NAME ": "
+#define AGSTEAM_PERSISTENT_DB "$SAVEGAMEDIR$/agsteam.db"
 
 #ifdef DEBUG
 #define debug_printf(args...) AGSteam_Printf("[DEBUG] " AGSTEAM_MESSAGE_PREFIX args)
@@ -41,6 +42,20 @@ BOOL APIENTRY DllMain(HANDLE hModule, DWORD ulReason, LPVOID lpReserved) {
 
 
 // Declarations
+
+// AGS internals
+struct AGSFile;
+enum FileMode {
+	eFileRead = 1,
+	eFileWrite = 2,
+	eFileAppend = 3
+};
+typedef AGSFile *(*AGSFileOpen)(const char *filename, int mode);
+typedef void (*AGSFileClose)(AGSFile *file);
+typedef int (*AGSFileReadInt)(AGSFile *file);
+typedef const char *(*AGSFileReadStringBack)(AGSFile *file);
+typedef void (*AGSFileWriteInt)(AGSFile *file, int value);
+typedef void (*AGSFileWriteString)(AGSFile *file, const char *value);
 
 // Global state
 static IAGSEngine *AGSteam_engine = 0;
@@ -59,6 +74,8 @@ static std::map<std::string, std::pair<float, float> > AGSteam_ratestats;
 static void AGSteam_Printf(const char *format, ...);
 static int AGSteam_New(IAGSEngine *engine);
 static void AGSteam_Delete();
+static bool AGSteam_LoadDb();
+static bool AGSteam_SaveDb();
 
 // Script interface declarations
 static int AGSteam_IsAchievementAchieved(const char *steamAchievementID);
@@ -105,6 +122,142 @@ static void AGSteam_Printf(const char *format, ...) {
 	va_end(args);
 }
 
+static bool AGSteam_LoadDb() {
+	bool loaded = false;
+	AGSFileOpen fileopen = reinterpret_cast<AGSFileOpen>(AGSteam_engine->GetScriptFunctionAddress("File::Open^2"));
+	AGSFileClose fileclose = reinterpret_cast<AGSFileClose>(AGSteam_engine->GetScriptFunctionAddress("File::Close^0"));
+	debug_printf("File.Open=%p File.Close=%p\n", fileopen, fileclose);
+	if (fileopen && fileclose) {
+		AGSFile *file = fileopen(AGSteam_engine->CreateScriptString(AGSTEAM_PERSISTENT_DB), eFileRead);
+		debug_printf("file=%p\n", file);
+		if (file) {
+			AGSFileReadInt readint = reinterpret_cast<AGSFileReadInt>(AGSteam_engine->GetScriptFunctionAddress("File::ReadInt^0"));
+			AGSFileReadStringBack readstring = reinterpret_cast<AGSFileReadStringBack>(AGSteam_engine->GetScriptFunctionAddress("File::ReadStringBack^0"));
+			debug_printf("File.ReadInt=%p File.ReadStringBack=%p\n", readint, readstring);
+			if (readint && readstring) {
+				std::string magic = readstring(file);
+				if (magic == AGSTEAM_PLUGIN_NAME) {
+					AGSteam_language = readstring(file);
+					AGSteam_username = readstring(file);
+					int leaderboards = readint(file);
+					for (int i = 0; i < leaderboards; i++) {
+						std::string name = readstring(file);
+						AGSteam_lbnames.push_back(name);
+						int score = readint(file);
+						AGSteam_lbscores[name] = score;
+					}
+					int achievements = readint(file);
+					for (int i = 0; i < achievements; i++) {
+						std::string name = readstring(file);
+						int achieved = readint(file);
+						AGSteam_achievements[name] = achieved ? true : false;
+					}
+					int intstats = readint(file);
+					for (int i = 0; i < intstats; i++) {
+						std::string name = readstring(file);
+						int stats = readint(file);
+						AGSteam_intstats[name] = stats;
+					}
+					int floatstats = readint(file);
+					for (int i = 0; i < floatstats; i++) {
+						std::string name = readstring(file);
+						union {
+							int from;
+							float to;
+						} stats;
+						stats.from = readint(file);
+						AGSteam_floatstats[name] = stats.to;
+					}
+					int ratestats = readint(file);
+					for (int i = 0; i < ratestats; i++) {
+						std::string name = readstring(file);
+						union {
+							int from;
+							float to;
+						} num, den;
+						num.from = readint(file);
+						den.from = readint(file);
+						AGSteam_ratestats[name] = std::make_pair(num.to, den.to);
+					}
+					loaded = true;
+				} else {
+					debug_printf("Error reading persistent data, invalid magic\n");
+				}
+			}
+			fileclose(file);
+		}
+	}
+	return loaded;
+}
+
+static bool AGSteam_SaveDb() {
+	bool saved = false;
+	AGSFileOpen fileopen = reinterpret_cast<AGSFileOpen>(AGSteam_engine->GetScriptFunctionAddress("File::Open^2"));
+	AGSFileClose fileclose = reinterpret_cast<AGSFileClose>(AGSteam_engine->GetScriptFunctionAddress("File::Close^0"));
+	debug_printf("File.Open=%p File.Close=%p\n", fileopen, fileclose);
+	if (fileopen && fileclose) {
+		AGSFile *file = fileopen(AGSteam_engine->CreateScriptString(AGSTEAM_PERSISTENT_DB), eFileWrite);
+		debug_printf("file=%p\n", file);
+		if (file) {
+			AGSFileWriteInt writeint = reinterpret_cast<AGSFileWriteInt>(AGSteam_engine->GetScriptFunctionAddress("File::WriteInt^1"));
+			AGSFileWriteString writestring = reinterpret_cast<AGSFileWriteString>(AGSteam_engine->GetScriptFunctionAddress("File::WriteString^1"));
+			debug_printf("File.WriteInt=%p File.WriteString=%p\n", writeint, writestring);
+			if (writeint && writestring) {
+				writestring(file, AGSteam_engine->CreateScriptString(AGSTEAM_PLUGIN_NAME));
+				writestring(file, AGSteam_engine->CreateScriptString(AGSteam_language.c_str()));
+				writestring(file, AGSteam_engine->CreateScriptString(AGSteam_username.c_str()));
+				writeint(file, AGSteam_lbnames.size());
+				for (std::vector<std::string>::const_iterator it = AGSteam_lbnames.begin(); it != AGSteam_lbnames.end(); it++) {
+					const std::string name = *it;
+					writestring(file, AGSteam_engine->CreateScriptString(name.c_str()));
+					writeint(file, AGSteam_lbscores[name]);
+				}
+				writeint(file, AGSteam_achievements.size());
+				for (std::map<std::string, bool>::const_iterator it = AGSteam_achievements.begin(); it != AGSteam_achievements.end(); it++) {
+					const std::string name = it->first;
+					bool achieved = it->second;
+					writestring(file, AGSteam_engine->CreateScriptString(name.c_str()));
+					writeint(file, achieved ? 1 : 0);
+				}
+				writeint(file, AGSteam_intstats.size());
+				for (std::map<std::string, int>::const_iterator it = AGSteam_intstats.begin(); it != AGSteam_intstats.end(); it++) {
+					const std::string name = it->first;
+					int stats = it->second;
+					writestring(file, AGSteam_engine->CreateScriptString(name.c_str()));
+					writeint(file, stats);
+				}
+				writeint(file, AGSteam_floatstats.size());
+				for (std::map<std::string, float>::const_iterator it = AGSteam_floatstats.begin(); it != AGSteam_floatstats.end(); it++) {
+					const std::string name = it->first;
+					union {
+						float from;
+						int to;
+					} stats;
+					stats.from = it->second;
+					writestring(file, AGSteam_engine->CreateScriptString(name.c_str()));
+					writeint(file, stats.to);
+				}
+				writeint(file, AGSteam_ratestats.size());
+				for (std::map<std::string, std::pair<float, float> >::const_iterator it = AGSteam_ratestats.begin(); it != AGSteam_ratestats.end(); it++) {
+					const std::string name = it->first;
+					union {
+						float from;
+						int to;
+					} num, den;
+					num.from = it->second.first;
+					den.from = it->second.second;
+					writestring(file, AGSteam_engine->CreateScriptString(name.c_str()));
+					writeint(file, num.to);
+					writeint(file, den.to);
+				}
+				saved = true;
+			}
+			fileclose(file);
+		}
+	}
+	return saved;
+}
+
 static int AGSteam_New(IAGSEngine *engine) {
 	if (!AGSteam_initialized) {
 		if (engine->version < 17) {
@@ -143,9 +296,12 @@ static int AGSteam_New(IAGSEngine *engine) {
 			AGSteam_engine->RegisterScriptFunction("AGSteam::get_LeaderboardCount", reinterpret_cast<void *>(AGSteam_LeaderboardCount));
 			AGSteam_engine->RegisterScriptFunction("AGSteam::GetCurrentGameLanguage^0", reinterpret_cast<void *>(AGSteam_GetCurrentGameLanguage));
 			AGSteam_engine->RegisterScriptFunction("AGSteam::GetUserName^0", reinterpret_cast<void *>(AGSteam_GetUserName));
+			if (AGSteam_LoadDb()) {
+				AGSteam_Printf(AGSTEAM_MESSAGE_PREFIX "Loaded persistent data\n");
+			}
 			AGSteam_initialized = true;
 			debug_printf("New(%p): %d\n", engine, AGSteam_initialized);
-			AGSteam_Printf(AGSTEAM_MESSAGE_PREFIX "Loaded\n");
+			AGSteam_Printf(AGSTEAM_MESSAGE_PREFIX "Loaded plugin\n");
 		}
 	}
 	return AGSteam_initialized ? 1 : 0;
@@ -153,7 +309,12 @@ static int AGSteam_New(IAGSEngine *engine) {
 
 static void AGSteam_Delete() {
 	debug_printf("Delete()\n");
-	AGSteam_Printf(AGSTEAM_MESSAGE_PREFIX "Unloaded\n");
+	if (AGSteam_initialized) {
+		if (AGSteam_SaveDb()) {
+			AGSteam_Printf(AGSTEAM_MESSAGE_PREFIX "Wrote persistent data\n");
+		}
+	}
+	AGSteam_Printf(AGSTEAM_MESSAGE_PREFIX "Unloaded plugin\n");
 	AGSteam_engine = 0;
 	AGSteam_initialized = false;
 }
